222db003d8508cdba6569b51b7a410d2bf413f83
Ahmed Shaaban <139641823+ahmedShaabanl@users.noreply.github.com>
2023-09-12 10:17:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const classActiv = document.getElementById("classActiv")
+function removeClass (e) {
+  
+  classActiv.classList.remove("active")
+}
+function addAni (e) {
+  const classActiv = document.getElementById("classActiv")
+  classActiv.classList.add("animation-open")
+}
+
+
+
+function addClass (e) {
+  const classActiv = document.getElementById("classActiv")
+  classActiv.classList.add("active")
+}
+
+
+
+
+
+
+// const aha = document.getElementsByClassName(".home-remove")
+
+// aha.onClick(addClass())
